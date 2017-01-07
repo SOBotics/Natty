a007d1b9afb69540ec5c16a5435f84d24c420eb8
FelixSFD <felix.deil@student.uni-augsburg.de>
2017-01-07 11:02:49 +0100

    + added different NAA-comments depending on the reputation

--- src/main/java/in/bhargavrao/stackoverflow/natty/utils/AutoCommentUtils.java
+++ src/main/java/in/bhargavrao/stackoverflow/natty/utils/AutoCommentUtils.java
@@ -17,21 +17,33 @@ public class AutoCommentUtils {
         Boolean hasNoCodeblock = false;
         Boolean containsBlacklistedWord = false;
         
-        for(String filter: report.getCaughtFor()){
+        /*for(String filter: report.getCaughtFor()){
             //filters to decide which auto-comment to use
-            if (filter.equalsIgnoreCase("No Code Block")) hasNoCodeblock = true;
-            if (filter.equalsIgnoreCase("Possible Link Only")) isPossibleLinkOnly = true;
-            if (filter.equalsIgnoreCase("Contains Blacklisted Word")) containsBlacklistedWord = true;
-        }        
+            if (!hasNoCodeblock && filter.equalsIgnoreCase("No Code Block")) hasNoCodeblock = true;
+            if (!isPossibleLinkOnly && filter.equalsIgnoreCase("Possible Link Only")) isPossibleLinkOnly = true;
+            if (!containsBlacklistedWord && filter.equalsIgnoreCase("Contains Blacklisted Word")) containsBlacklistedWord = true;
+        }*/
         
+        if (report.getCaughtFor().contains("No Code Block")) hasNoCodeblock = true;
+        if (report.getCaughtFor().contains("Possible Link Only")) isPossibleLinkOnly = true;
+        if (report.getCaughtFor().contains("Contains Blacklisted Word")) containsBlacklistedWord = true;
+                
         //decide, which comment to use
         if (hasNoCodeblock && isPossibleLinkOnly && !containsBlacklistedWord) {
         	//link-only
         	System.out.println("link-only");
         	return new AutoComment(AutoCommentType.LINK_ONLY);
         } else {
-        	System.out.println("naa");
-        	return new AutoComment(AutoCommentType.NAA);
+        	System.out.println("Some NAA");
+        	//check the reputation to provide different insctructions for users that can't comment yet
+        	if (report.getPost().getAnswerer().getReputation() < 50) {
+        		//not enough rep to comment
+        		System.out.println("low rep");
+        		return new AutoComment(AutoCommentType.NAA_LOW_REP);
+        	} else {
+        		System.out.println("high rep");
+        		return new AutoComment(AutoCommentType.NAA_HIGH_REP);
+        	}
         }
 	}
 }
