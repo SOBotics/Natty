c607caac3ea32322f1572b7da8f1c82fed5b6d5a
FelixSFD <felix.deil@student.uni-augsburg.de>
2017-01-17 19:53:44 +0100

    + added comment for NAA rep > 50

--- src/main/java/in/bhargavrao/stackoverflow/natty/entities/autocomments/AutoCommentNAAHighRep.java
+++ src/main/java/in/bhargavrao/stackoverflow/natty/entities/autocomments/AutoCommentNAAHighRep.java
@@ -9,7 +9,7 @@ public class AutoCommentNAAHighRep implements AutoComment {
 
 	@Override
 	public String getText() {
-		return "NAA for >=50 rep";
+		return "This post isn't an actual attempt at answering the question. Please note [Stack Overflow doesn't work like a discussion forum](http://stackoverflow.com/tour), it is a Q&A site where every post is either a question or an answer to a question. Posts can also have [comments](http://stackoverflow.com/help/privileges/comment) - small sentences like this one - that can be used to critique or request clarification from an author. This should be either a comment or [a new question](http://stackoverflow.com/questions/ask).";
 	}
 
 }
