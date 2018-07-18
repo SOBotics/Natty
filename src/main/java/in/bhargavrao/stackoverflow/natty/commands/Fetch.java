0f6621d926061d8142d2a05f60ba1d251292f429
Bhargav-Rao <bhargav.rao448@gmail.com>
2018-07-17 23:17:34 -0700

    Did a complete refactor of the `fetch` command.

--- src/main/java/in/bhargavrao/stackoverflow/natty/commands/Fetch.java
+++ src/main/java/in/bhargavrao/stackoverflow/natty/commands/Fetch.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import in.bhargavrao.stackoverflow.natty.services.ApiService;
 import in.bhargavrao.stackoverflow.natty.services.FileStorageService;
+import in.bhargavrao.stackoverflow.natty.services.PropertyService;
 import in.bhargavrao.stackoverflow.natty.services.StorageService;
 import in.bhargavrao.stackoverflow.natty.utils.CheckUtils;
 import in.bhargavrao.stackoverflow.natty.utils.CommandUtils;
@@ -47,114 +48,160 @@ public class Fetch implements SpecialCommand {
 
         String data = CommandUtils.extractData(message.getPlainContent()).trim();
         StorageService service = new FileStorageService();
+        PropertyService propertyService = new PropertyService();
         try{
             List<String> lines = service.getReports(sitename);
             String returnString = String.join("; ", lines);
 
-            if(data.split(" ")[0].equals("links") && lines.size()!=0) {
-                if (lines.size() == 0)
-                    room.replyTo(message.getId(), "All reports have been tended to");
-                else {
-                    int stopValue = lines.size();
-                    if(data.split(" ").length==2&&CheckUtils.checkIfInteger(data.split(" ")[1])){
-                        stopValue = Integer.parseInt(data.split(" ")[1]);
-                    }
 
-                    String links = "";
+            if (lines.size() == 0) {
+                room.send("All reports have been tended to");
+                return;
+            }
+
+            String[] splits = data.split(" ");
+            String option;
+            try {
+                option = splits[0].toLowerCase();
+            }
+            catch (ArrayIndexOutOfBoundsException e){
+                room.send(returnString);
+                return;
+            }
+
+            int stopValue = lines.size();
+            if(splits.length==2&&CheckUtils.checkIfInteger(splits[1])){
+                stopValue = Integer.parseInt(splits[1]);
+            }
+
+
+            switch (option) {
+                case "links": {
+
+                    StringBuilder links = new StringBuilder();
                     int i = 0;
-                    for(String line: lines) {
-                        links += "["+line.trim()+"](//"+siteurl+"/a/"+line.trim()+"); ";
+                    for (String line : lines) {
+                        links.append("[").append(line.trim()).append("](//").append(siteurl).append("/a/").append(line.trim()).append("); ");
                         i++;
-                        if(i==stopValue)
+                        if (i == stopValue)
                             break;
                     }
-                    room.replyTo(message.getId(), links);
+                    room.send(links.toString());
+                    break;
                 }
-            }
-            else if(data.split(" ")[0].equals("FMS") && lines.size()!=0) {
+                case "fms": {
 
-                String links = "";
-                for(String line: lines) {
-                    links += "["+line.trim()+"](http://51.254.218.90:8000/Natty/"+line.trim()+".html); ";
-                }
-                room.replyTo(message.getId(), links);
-            }
-            else if(data.split(" ")[0].equals("deleted") && lines.size()!=0) {
-                if(lines.size()>100)
-                {
-                    room.send("There are more than 100 requests. Hence fetching deleted posts from the first 100 only");
-                    lines = lines.subList(0,100);
-                }
-                ApiService apiService = new ApiService(siteurl);
-                List<Integer> answerIds = lines.stream().map(Integer::parseInt).collect(Collectors.toList());
-                JsonObject answersJson = apiService.getAnswerDetailsByIds(answerIds);
+                    StringBuilder links = new StringBuilder();
 
-                if(answersJson.has("items")){
-                    JsonArray answers = answersJson.get("items").getAsJsonArray();
-                    for (JsonElement element: answers){
-                        JsonObject answer = element.getAsJsonObject();
-                        String answerId = answer.get("answer_id").getAsString();
-                        int index = lines.indexOf(answerId);
-                        if (index!=-1){
-                            lines.remove(index);
+                    propertyService.getFMSUrl();
+                    int i = 0;
+                    for (String line : lines) {
+
+                        String postId = line.trim();
+                        String sentinelId = service.getSentinelId(postId, sitename);
+                        if (sentinelId==null || sentinelId.equals("-1")) {
+                            links.append("[").append(line.trim()).append("](").append(propertyService.getFMSUrl()).append("/").append(line.trim()).append(".html); ");
+                        }
+                        else {
+                            links.append(postId).append("; ");
+                        }
+                        i++;
+                        if (i == stopValue)
+                            break;
+                    }
+                    room.send(links.toString());
+                    break;
+                }
+                case "deleted": {
+                    if (lines.size() > 100 && stopValue > 100) {
+                        room.send("There are more than 100 requests. Hence fetching deleted posts from the first 100 only");
+                        lines = lines.subList(0, 100);
+                    }
+
+                    ApiService apiService = new ApiService(siteurl);
+                    List<Integer> answerIds = lines.stream().map(Integer::parseInt).collect(Collectors.toList());
+                    JsonObject answersJson = apiService.getAnswerDetailsByIds(answerIds);
+
+                    if (answersJson.has("items")) {
+                        JsonArray answers = answersJson.get("items").getAsJsonArray();
+                        for (JsonElement element : answers) {
+                            JsonObject answer = element.getAsJsonObject();
+                            String answerId = answer.get("answer_id").getAsString();
+                            int index = lines.indexOf(answerId);
+                            if (index != -1) {
+                                lines.remove(index);
+                            }
                         }
                     }
-                }
-                String links = "";
-                for(String line: lines) {
-                    links += "["+line.trim()+"](//"+siteurl+"/a/"+line.trim()+"); ";
-                }
-
-                if(links.trim().equals(""))
-                    links = "There are no deleted posts";
-                room.replyTo(message.getId(), links);
-            }
-            else if(data.split(" ")[0].equals("nondeleted") && lines.size()!=0) {
-                if(lines.size()>100)
-                {
-                    room.send("There are more than 100 requests. Hence fetching non deleted posts from the first 100 only");
-                    lines = lines.subList(0,100);
-                }
-                ApiService apiService = new ApiService(siteurl);
-                List<Integer> answerIds = lines.stream().map(Integer::parseInt).collect(Collectors.toList());
-                JsonObject answersJson = apiService.getAnswerDetailsByIds(answerIds);
-                String links = "";
-
-                if(answersJson.has("items")){
-                    JsonArray answers = answersJson.get("items").getAsJsonArray();
-                    for (JsonElement element: answers){
-                        JsonObject answer = element.getAsJsonObject();
-                        String answerId = answer.get("answer_id").getAsString();
-                        links += "["+answerId+"](//"+siteurl+"/a/"+answerId+"); ";
+                    StringBuilder links = new StringBuilder();
+                    int i = 0;
+                    for (String line : lines) {
+                        links.append("[").append(line.trim()).append("](//").append(siteurl).append("/a/").append(line.trim()).append("); ");
+                        i++;
+                        if (i == stopValue)
+                            break;
                     }
-                }
 
-                if(links.trim().equals(""))
-                    links = "There are no non deleted posts";
-                room.replyTo(message.getId(), links);
-            }
-            else if(data.split(" ")[0].toLowerCase().equals("sentinel") && lines.size()!=0) {
-                String links = "";
-                for(String line: lines) {
-                    String postId = line.trim();
-                    String sentinelId = service.getSentinelId(sitename,postId);
-                    sentinelId = sentinelId.replace(postId+",","");
-                    if(sentinelId.equals("-1"))
-                        links+= postId+"; ";
-                    else
-                        links+= "["+postId+"]("+SentinelUtils.getSentinelMainUrl(sitename)+"/posts/"+sentinelId+"); ";
+                    if (links.toString().trim().equals(""))
+                        links = new StringBuilder("There are no deleted posts");
+                    room.send(links.toString());
+                    break;
                 }
-                room.replyTo(message.getId(), links);
-            }
-            else if((data.split(" ")[0].equals("amount") || data.split(" ")[0].equals("count") || data.split(" ")[0].equals("number"))  && lines.size()!=0) {
-                room.replyTo(message.getId(), Integer.toString(lines.size()));
-            }
-            else {
+                case "nondeleted": {
+                    if (lines.size() > 100 && stopValue > 100) {
+                        room.send("There are more than 100 requests. Hence fetching non deleted posts from the first 100 only");
+                        lines = lines.subList(0, 100);
+                    }
+                    ApiService apiService = new ApiService(siteurl);
+                    List<Integer> answerIds = lines.stream().map(Integer::parseInt).collect(Collectors.toList());
+                    JsonObject answersJson = apiService.getAnswerDetailsByIds(answerIds);
+                    StringBuilder links = new StringBuilder();
 
-                if (lines.size() == 0)
-                    room.replyTo(message.getId(), "All reports have been tended to");
-                else
-                    room.replyTo(message.getId(), returnString);
+                    int i = 0;
+                    if (answersJson.has("items")) {
+                        JsonArray answers = answersJson.get("items").getAsJsonArray();
+                        for (JsonElement element : answers) {
+                            JsonObject answer = element.getAsJsonObject();
+                            String answerId = answer.get("answer_id").getAsString();
+                            links.append("[").append(answerId).append("](//").append(siteurl).append("/a/").append(answerId).append("); ");
+                            i++;
+                            if (i == stopValue)
+                                break;
+                        }
+                    }
+
+                    if (links.toString().trim().equals(""))
+                        links = new StringBuilder("There are no non deleted posts");
+                    room.send(links.toString());
+                    break;
+                }
+                case "sentinel": {
+                    StringBuilder links = new StringBuilder();
+                    int i = 0;
+                    for (String line : lines) {
+                        String postId = line.trim();
+                        String sentinelId = service.getSentinelId(postId, sitename);
+                        if (sentinelId==null || sentinelId.equals("-1")) {
+                            links.append(postId).append("; ");
+                        } else {
+                            sentinelId = sentinelId.replace(postId + ",", "");
+                            links.append("[").append(postId).append("](").append(SentinelUtils.getSentinelMainUrl(sitename)).append("/posts/").append(sentinelId).append("); ");
+                        }
+                        i++;
+                        if (i == stopValue)
+                            break;
+                    }
+                    room.send(links.toString());
+                    break;
+                }
+                case "amount":
+                case "count":
+                case "number":
+                    room.send(Integer.toString(lines.size()));
+                    break;
+                default:
+                    room.send(returnString);
+                    break;
             }
         }
         catch (IOException e){
